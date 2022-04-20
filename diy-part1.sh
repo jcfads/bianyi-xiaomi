a8098e557eabed6e0947203ab561d65ef7a2bbd6
jcfads <47596409+jcfads@users.noreply.github.com>
2022-04-20 08:42:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,5 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
  Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+git clone https://github.com/kenzok8/openwrt-packages package/1
+git clone https://github.com/xiaorouji/openwrt-passwall package/2
